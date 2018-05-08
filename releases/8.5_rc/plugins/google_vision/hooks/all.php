dea47f0e92cfae4b708e0ae11473de9483315961
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-05-08 09:55:18 +0000

    CHG: Cleanup, deleting unnecessary RC branches [notag]

--- releases/8.5_rc/plugins/google_vision/hooks/all.php
+++
@@ -1,7 +0,0 @@
-<?php
-function HookGoogle_visionAllAfterpreviewcreation($resource)
-    {
-    include_once __DIR__ . "/../include/google_vision_functions.php";
-
-    google_visionProcess($resource);
-    }
