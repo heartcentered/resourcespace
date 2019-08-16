cbd9a92700e0a1d84c828aeee19c28e0b30fe4b4
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-08-16 15:59:49 +0000

    NEW: Creating branch for CSS and visual improvements [q10889]

---
+++ branches/20190816_jbrooke_q10889/plugins/google_vision/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$google_vision_api_key="";
+$google_vision_label_field=73;
+$google_vision_landmarks_field=1;
+$google_vision_text_field=18;
+$google_vision_restypes=array("1");
+$google_vision_features=array("LABEL_DETECTION","LANDMARK_DETECTION","TEXT_DETECTION");
+$google_vision_autotitle=false;
+$google_vision_face_detect_field=0;
+$google_vision_face_detect_verbose=false;
+$google_vision_face_detect_fullface=true;
+$google_vision_face_dependent_field=0;
