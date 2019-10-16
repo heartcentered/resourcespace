2766b5e68d898798a9e028e843cf8aaa476d5436
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-10-16 15:36:32 +0000

    CHG: bringing bringing branch up to date [q10489][branches/20191004_acota_col_undo_q10489]

--- branches/20191004_acota_col_undo_q10489/css/css_override.php
+++ branches/20191004_acota_col_undo_q10489/css/css_override.php
@@ -12,6 +12,7 @@ header("Content-type: text/css");
 
 global $header_colour_style_override, $header_link_style_override, $home_colour_style_override, $collection_bar_background_override,
 $collection_bar_foreground_override;
+
 $browse_on = has_browsebar();
 
 # Override the header background colour
@@ -66,14 +67,28 @@ if ((isset($collection_bar_background_override) && $collection_bar_background_ov
     <?php
     }
 
-# Override the collection bar foreground colour
+/**
+ * Override the collection bar foreground colour
+ * 
+ * optgroup and option background-color set to #474747 for consistency across platforms as 
+ * Firefox/Windows does not recognise rgb() colour properties for optgroup element
+ * */
+
 if ((isset($collection_bar_foreground_override) && $collection_bar_foreground_override != ''))
     {
     ?>
     .CollectionPanelShell, #CollectionDiv select
         {
         background-color: <?php echo $collection_bar_foreground_override; ?>;
-        }
+        }    
+    
+    #CollectionDiv option, #CollectionDiv optgroup 
+        {
+        font-style:normal;
+        background-color: #474747;
+        color: #fff;
+        }    
+
 
     .ui-layout-resizer
         {
