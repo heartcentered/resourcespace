7f5a6688b2684108e9ab22fd7c027e1d277b2e30
tgray <tgray@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-10-25 14:06:53 +0000

    FIX: 9.1 - update - add share resource functionality for remote resourceconnect resources in a local collection [t21278]

--- releases/9.1/plugins/resourceconnect/hooks/search.php
+++ releases/9.1/plugins/resourceconnect/hooks/search.php
@@ -129,6 +129,7 @@ function HookResourceConnectSearchThumblistextras()
     $title = $resource["field8"]; // image title
     ?>
     <!-- Full screen preview -->
+    <a aria-hidden="true" class="fa fa-expand" id="previewlinkcollection<?php echo $ref ?>" href="<?php echo $pre_url ?>" title="Full screen preview" data-title="<?php echo $lang["fullscreenpreview"] ?>" data-lightbox="lightboxcollection" onmouseup="closeModalOnLightBoxEnable();"></a>
  <!-- Share resource -->
  <a aria-hidden="true" class="fa fa-share-alt"
                                 href="<?php echo $baseurl_short?>plugins/resourceconnect/pages/resource_share.php?url=<?php echo urlencode($url) ?>"  
@@ -142,7 +143,7 @@ function HookResourceConnectSearchThumblistextras()
  <?php
     }
 
-    
+
 function HookResourceConnectSearchProcess_search_results($result,$search)
     {
     global $baseurl,$k;
