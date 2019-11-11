3e712436c014fa6bc1ddfad5274a8f70913fa168
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-11-11 10:19:53 +0000

    NEW: Creating branch to download system configuration [q10765]

---
+++ branches/20191111_nbraddy_download_config_q10765/plugins/rse_version/languages/en.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang["logconfirmrevert"]="Are you sure you wish to revert this change?";
+$lang["revertingwillapply"]="Reverting will apply the change:";
+$lang["revertingwillreplace"]="Reverting will replace the current file with:";
+$lang["revertingclicktoproceed"]="The following changes will be applied:";
+$lang["revertingwilladd"]="These options will be added:";
+$lang["revertingwillremove"]="These options will be removed:";
+$lang["revertingwillignore"]="These obsolete options will be ignored:";
+$lang["revert_log_note"]="(revert)";
+$lang["revertmetadatatodatetime"]="Revert to state as of date/time (YYYY-MM-DD HH:MM)";
+$lang["logdownloadearlierversion"]="Download this earlier version of the file";
+$lang["rse_version_revert_state"] = "Revert state";
+$lang["rse_version_rstate_changes"] = "Reverting state for collection #%COLLECTION by replaying events up to, but not including '%DATE' for resource #%RESOURCE.";
