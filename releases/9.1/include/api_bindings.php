9be366f82808caba298520417fc125958d828c3a
jhardy <jhardy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-11-11 11:30:03 +0000

    NEW: [9.1] get_node_id() function to retrieve a node ID for a given node name; exposed via API [t21356]

--- releases/9.1/include/api_bindings.php
+++ releases/9.1/include/api_bindings.php
@@ -594,3 +594,9 @@ function api_resource_log_last_rows($minref = 0, $days = 7, $maxrecords = 0)
     return resource_log_last_rows($minref, $days, $maxrecords);
     }    
 
+function api_get_node_id($value, $resource_type_field)
+    {
+    if(!metadata_field_view_access($resource_type_field)) {return false;} # Need at least view access to the field.
+
+    return get_node_id($value,$resource_type_field);
+    }
