85aec5d2bf28b6ea2f0a712284228e811b75f32e
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-12-09 16:49:51 +0000

    NEW: ability to remotely get the original files for the SRC resources [q10964][branches/20191029_acota_q10964]

--- include/job_handlers/upload_processing.php
+++ include/job_handlers/upload_processing.php
@@ -6,13 +6,37 @@ include_once __DIR__ .  "/../resource_functions.php";
 # $job_data["revert"]
 # $job_data["autorotate"]
 # $job_data["archive"] -> optional based on $upload_then_process_holding_state
+# $job_data["upload_file_by_url"] -> optional. If NOT empty, means upload_file_by_url should be used instead
+
+$upload_file_by_url = isset($job_data["upload_file_by_url"]) && is_string($job_data["upload_file_by_url"]) ? $job_data["upload_file_by_url"] : "";
+
+// Set up the user who triggered this event - the upload should be done as them
+$user_data = validate_user("AND u.ref = '" . escape_check($job['user']) . "'", true);
+if(!is_array($user_data) || count($user_data) == 0)
+    {
+    job_queue_update($jobref, $job_data, STATUS_ERROR);
+    return;
+    }
+setup_user($user_data[0]);
 
 $resource=get_resource_data($job_data["resource"]);
 $status=false;
 
 if($resource!==false)
 	{
-	$status=upload_file($job_data["resource"], $job_data["extract"], $job_data["revert"], $job_data["autorotate"] ,"", true);
+    if(trim($upload_file_by_url) != "")
+        {
+        $status = upload_file_by_url(
+            $job_data["resource"],
+            $job_data["extract"],
+            $job_data["revert"],
+            $job_data["autorotate"],
+            $job_data["upload_file_by_url"]);
+        }
+    else
+        {
+        $status=upload_file($job_data["resource"], $job_data["extract"], $job_data["revert"], $job_data["autorotate"] ,"", true);
+        }
 	
 	# update the archive status
 	if(isset($job_data['archive']) && $job_data['archive'] !== '')
