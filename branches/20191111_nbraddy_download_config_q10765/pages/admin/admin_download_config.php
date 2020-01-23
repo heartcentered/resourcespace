00fc805f4f07b8e17afc191e834be61c73321179
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-01-23 08:26:59 +0000

    NEW: Updated code to download config [q10765]

--- branches/20191111_nbraddy_download_config_q10765/pages/admin/admin_download_config.php
+++ branches/20191111_nbraddy_download_config_q10765/pages/admin/admin_download_config.php
@@ -24,74 +24,7 @@ $separatesql = getval("separatesql","") !== "";
 
 if (!isset($error) && $export!="" && enforcePostRequest(false))
 	{
-    // Create array of tables to export
-    $exporttables = array();
-    $exporttables["sysvars"] = array();
-    $exporttables["preview_size"] = array();
-    $exporttables["archive_states"] = array();
-    $exporttables["workflow_actions"] = array();
-
-    $exporttables["user"] = array();
-    $exporttables["user"]["scramble"]=array("username"=>"mix_text","email"=>"mix_email","fullname"=>"mix_text","comments"=>"mix_text","created"=>"mix_date");
-    $exporttables["user_preferences"] = array();
-
-    $exporttables["usergroup"] = array();
-    $exporttables["usergroup"]["scramble"]=array("name"=>"mix_text","welcome_message"=>"mix_text","search_filter"=>"mix_text","edit_filter"=>"mix_text");
-
-
-    $exporttables["dash_tile"] = array();
-    $exporttables["dash_tile"]["scramble"]=array("title"=>"mix_text","txt"=>"mix_text","url"=>"mix_url");
-    $exporttables["user_dash_tile"] = array();
-    $exporttables["usergroup_dash_tile"] = array();
-
-    $exporttables["resource_type"] = array();
-    $exporttables["resource_type_field"] = array();
-    $exporttables["resource_type_field"]["scramble"]=array("title"=>"mix_text","name"=>"mix_text");
-
-    $exporttables["node"] = array();
-    $exporttables["node"]["scramble"]=array("name"=>"mix_text");
-
-    $exporttables["filter"] = array();
-    $exporttables["filter"]["scramble"]=array("name"=>"mix_text");
-    $exporttables["filter_rule"] = array();
-    $exporttables["filter_rule_node"] = array();
-
-    // Optional tables
-    if($exportcollection != 0)
-        {
-        // Collections 
-        $exporttables["collection"] = array();
-        $exporttables["collection"]["exportcondition"] = "WHERE ref = '$exportcollection'";    
-        $exporttables["collection"]["scramble"]=array("name"=>"mix_text","description"=>"mix_text","keywords"=>"mix_text","theme"=>"mix_text","theme2"=>"mix_text","theme3"=>"mix_text","theme4"=>"mix_text","theme5"=>"mix_text","created"=>"mix_date");
-
-        $exporttables["user_collection"] = array();
-        $exporttables["usergroup_collection"] = array();
-        $exporttables["collection_resource"] = array(); 
-        //  Resources and resource metadata
-        $exporttables["resource"] = array();
-        $exporttables["resource"]["scramble"]=array("field8"=>"mix_text","creation_date"=>"mix_date");
-        $exporttables["resource"]["exportcondition"] = " WHERE ref IN (SELECT resource FROM collection_resource WHERE collection='$exportcollection')";
-        $exporttables["resource_data"] = array();
-        $exporttables["resource_data"]["scramble"]=array("value");
-        $exporttables["resource_data"]["exportcondition"] = " WHERE resource IN (SELECT resource FROM collection_resource WHERE collection='$exportcollection')";
-        $exporttables["resource_data"]["scramble"]=array("value"=>"mix_text");
-
-        $exporttables["resource_node"] = array();
-        $exporttables["resource_custom_access"] = array();
-        $exporttables["resource_dimensions"] = array();
-        $exporttables["resource_related"] = array();
-        $exporttables["resource_alt_files"] = array();
-        $exporttables["resource_alt_files"]["scramble"]=array("name"=>"mix_text","description"=>"mix_text","file_name"=>"mix_filename");
-        $exporttables["annotation"] = array();
-        $exporttables["annotation_node"] = array();
-        }
-
-    $extra_tables = hook("export_add_tables");
-    if(is_array($extra_tables))
-        {
-        $exporttables = array_merge($exporttables,$extra_tables);
-        }
-    
+    $exporttables = get_export_tables($exportcollection);
 
     // Create offline job
     $job_data=array();
