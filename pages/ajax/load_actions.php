24ff7f80413a8494301df9a90dd7bb300da7364f
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-05-06 16:36:50 +0000

    CHG: Loading actions via AJAX to cater for selection collection needs [branches/20200424_acota_q10644][q10644]

--- pages/ajax/load_actions.php
+++ pages/ajax/load_actions.php
@@ -17,10 +17,94 @@ switch ($type)
     case "collection":
         hook('render_themes_list_tools', '', $ref);
         $collection_data = get_collection($ref);
-        //print_r($collection_data);
         render_actions($collection_data,false,false,$ref,array(),true, $forpage);
     break;
     
+    case "selection_collection":
+        // @todo: make this a function as we need it on search when we already have selected resources.
+        $search = "!collection{$USER_SELECTION_COLLECTION}";
+        $restypes = getvalescaped("restypes", "");
+        $order_by = getvalescaped("order_by", "relevance");
+        $archive = getvalescaped("archive", "0");
+        $per_page = getvalescaped("per_page", null, true);
+        $offset = getvalescaped("offset", null, true);
+        $fetchrows = (!is_null($per_page) || !is_null($offset) ? $per_page + $offset : -1);
+        $sort = getvalescaped("sort", "desc");
+        // $access_override = false;
+        $starsearch = getvalescaped("starsearch", 0, true);
+        // $ignore_filters = false;
+        // $return_disk_usage = false;
+        $recent_search_daylimit = getvalescaped("recent_search_daylimit", "");
+        $go = getvalescaped("go", "");
+        // $stats_logging = true;
+        // $return_refs_only = false;
+        $editable_only = getvalescaped("foredit","")=="true";
+
+        // $result = do_search(
+        //     $search,
+        //     $restypes,
+        //     $order_by,
+        //     $archive,
+        //     $fetchrows,
+        //     $sort,
+        //     false,
+        //     $starsearch,
+        //     false,
+        //     false,
+        //     $recent_search_daylimit,
+        //     $go, 
+        //     true, 
+        //     false, 
+        //     $editable_only);
+        // $resources_count = count($result);
+
+        $resources_count = get_collection_resources($USER_SELECTION_COLLECTION);
+        $collection_data = get_collection($USER_SELECTION_COLLECTION);
+
+        $valid_selection_collection_actions = array(
+            "relate_all",
+            "save_search_items_to_collection",
+            "remove_selected_from_collection",
+            "search_items_disk_usage",
+            "csv_export_results_metadata",
+            // todo: add share selction
+        );
+
+        $render_actions_extra_options = array(
+            array(
+                "value" => "remove_selected_from_collection",
+                "label" => $lang["remove_selected_from_collection"],
+                "data-attr" => array(
+                    "url" => generateURL(
+                        "{$baseurl_short}pages/collections.php",
+                        array(
+                            "removesearch" => $search,// find how to remove search from collection (if it exists)
+                            "restypes" => $restypes,
+                            "order_by" => $order_by,
+                            "sort" => $sort,
+                            "archive" => $archive,
+                            "mode" => "resources",
+                            "starsearch" => $starsearch,
+                        )
+                    )
+                ),
+                "category" => ACTIONGROUP_COLLECTION,
+            ),
+        );
+        $render_actions_filter = function($action) use ($valid_selection_collection_actions)
+            {
+            return in_array($action["value"], $valid_selection_collection_actions);
+            };
+
+        // override the language for actions as it's now specific to a selection of resources
+        $lang["relateallresources"] = $lang["relate_selected_resources"];
+        $lang["savesearchitemstocollection"] = $lang["add_selected_to_collection"];
+        $lang["searchitemsdiskusage"] = $lang["selected_items_disk_usage"];
+        $lang["csvExportResultsMetadata"] = $lang["csvExportResultsMetadata"];
+
+        render_actions($collection_data, true, false);
+        break;
+
     case "resource":
     break;
     }
