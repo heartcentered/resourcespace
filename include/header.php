7093004208ff312e46bbb29c6da0931215f6209e
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-05-07 13:06:34 +0000

    NEW: ability to remove selected resources from user collection [branches/20200424_acota_q10644][q10644]

--- include/header.php
+++ include/header.php
@@ -230,6 +230,8 @@ if($browse_on)
     browse_clicked = false;";     
     }
 ?>
+
+var usercollection = <?php echo (int) $usercollection; ?>;
 </script>
 
 <script src="<?php echo $baseurl_short?>lib/js/global.js?css_reload_key=<?php echo $css_reload_key?>" type="text/javascript"></script>
