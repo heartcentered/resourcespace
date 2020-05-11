02dfc8d940062ae670ace55fc4df6e3279a11d64
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-05-11 13:27:14 +0000

    NEW: Creating branch for API - Named Parameters [q10491]

---
+++ branches/20200511_acota_q10491/plugins/licensemanager/hooks/all.php
@@ -0,0 +1,66 @@
+<?php
+
+function HookLicensemanagerAllExport_add_tables()
+    {
+    return array("license"=>array("scramble"=>array("holder","license_usage","description")));
+    return array("resource_license"=>array("scramble"=>array()));
+    }
+
+function HookLicensemanagerAllRender_actions_add_collection_option($top_actions,array $options)
+    {
+    // Add the options to link a license and unlike the license
+    global $search,$lang,$k,$baseurl_short;
+
+    if($k != '' || !checkperm("a"))
+        {
+        return array();
+        }
+
+    if(isset($GLOBALS["hook_return_value"]) && is_array($GLOBALS["hook_return_value"]))
+        {
+        // @see hook() for an explanation about the hook_return_value global
+        $options = $GLOBALS["hook_return_value"];
+        }
+
+    $data_attr_url = generateURL(
+        $baseurl_short . "plugins/licensemanager/pages/batch.php",
+        array(
+            'collection'   => $search,
+            'unlink' => 'true'
+        )
+    );
+
+    $option = array(
+        'value'     => 'license_batch',
+        'label'     => $lang['unlinklicense'],
+        'data_attr' => array(
+            'url' => $data_attr_url,
+        ),
+        'category' => ACTIONGROUP_ADVANCED
+    );
+
+
+    array_push($options, $option);
+
+    $data_attr_url = generateURL(
+        $baseurl_short . "plugins/licensemanager/pages/batch.php",
+        array(
+            'collection'   => $search
+        )
+    );
+
+    $option = array(
+        'value'     => 'license_batch',
+        'label'     => $lang['linklicense'],
+        'data_attr' => array(
+            'url' => $data_attr_url,
+        ),
+        'category' => ACTIONGROUP_ADVANCED
+    );
+
+    array_push($options, $option);
+
+
+
+    return $options;
+    }
